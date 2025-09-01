1ba6557e0dc34b6089a90a84d3b64f62b9369460
김민준 <minjun@gimminjun-ui-MacBookAir.local>
2025-09-01 18:35:49 +0900

    recipe backend connection

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,8 @@
 module.exports = function override(config, env) {
+    // 웹팩 개발 서버 설정에서 'allowedHosts'를 'auto'로 변경하여
+    // Invalid options object 에러를 해결합니다.
+    if (config.devServer) {
+        config.devServer.allowedHosts = 'auto';
+    }
     return config;
 };
